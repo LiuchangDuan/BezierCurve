ba36b4ed5b18dca969d1fcd1e7005b179ee7e0b4
LiuchangDuan <870121425@qq.com>
2016-11-16 21:31:11 +0800

    circle -> heart

---
+++ heartview/src/main/java/com/example/heartview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.heartview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new Bezier(this));
+//        setContentView(R.layout.activity_main);
+    }
+}
