43c77ba53e80bae115f6c7050585192099cd0a16
LiuchangDuan <870121425@qq.com>
2016-11-20 23:11:20 +0800

    DST_IN and DST_OUT

--- porterduffviewdemo/src/main/java/com/example/porterduffviewdemo/MainActivity.java
+++ porterduffviewdemo/src/main/java/com/example/porterduffviewdemo/MainActivity.java
@@ -8,7 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new PorterDuffView(this));
+        setContentView(new DisOutView(this));
+//        setContentView(new DisInView(this));
+//        setContentView(new PorterDuffView(this));
 //        setContentView(R.layout.activity_main);
     }
 }
